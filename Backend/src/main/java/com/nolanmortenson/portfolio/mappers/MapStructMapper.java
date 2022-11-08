60be2761538d4be80eea740d444f11a2247a173e
Nolan Mortenson <nolan.mortenson@gmail.com>
2022-11-08 14:59:22 -0500

    implementing service layer for AppUser

--- Backend/src/main/java/com/nolanmortenson/portfolio/mappers/MapStructMapper.java
+++ Backend/src/main/java/com/nolanmortenson/portfolio/mappers/MapStructMapper.java
@@ -4,7 +4,11 @@ import com.nolanmortenson.portfolio.dto.AppUserDTO;
 import com.nolanmortenson.portfolio.models.AppUser;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MapStructMapper {
     AppUserDTO appUserToDto(AppUser appUser);
+
+    List<AppUserDTO> appUserListToDtoList(List<AppUser> appUsers);
 }
--- Backend/src/main/java/com/nolanmortenson/portfolio/services/AppUserServiceImpl.java
+++ Backend/src/main/java/com/nolanmortenson/portfolio/services/AppUserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.nolanmortenson.portfolio.services;
 
 import com.nolanmortenson.portfolio.dto.AppUserDTO;
+import com.nolanmortenson.portfolio.mappers.MapStructMapper;
 import com.nolanmortenson.portfolio.models.AppUser;
+import com.nolanmortenson.portfolio.repositories.AppUserRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,29 +15,55 @@ import java.util.List;
 @Slf4j
 public class AppUserServiceImpl implements AppUserService{
 
-
+    AppUserRepo appUserRepo;
+    MapStructMapper mapper;
     @Override
     public List<AppUserDTO> getAllUsers() {
-        return null;
+        return mapper.appUserListToDtoList(appUserRepo.findAll());
     }
 
     @Override
     public AppUserDTO getUserById(Long id) {
-        return null;
+        return mapper.appUserToDto(appUserRepo.findById(id).orElse(null));
     }
 
     @Override
     public AppUserDTO createNewUser(AppUser appUser) {
-        return null;
+        if(appUserRepo.existsById(appUser.getId())){
+            //throw error
+        }
+
+        AppUser convert = appUserRepo.save(appUser);
+
+        return mapper.appUserToDto(convert);
     }
 
     @Override
-    public AppUserDTO updateUser(AppUser appUser) {
-        return null;
+    public AppUserDTO updateUser(AppUser appUserUpdate) {
+       if(!appUserRepo.existsById(appUserUpdate.getId()))
+       {
+           //throw error
+       }
+
+       AppUser appUser = appUserRepo.findById(appUserUpdate.getId()).orElse(null);
+
+       appUser.setEmail(appUserUpdate.getEmail());
+       appUser.setFirstName(appUserUpdate.getFirstName());
+       appUser.setLastName(appUserUpdate.getLastName());
+       appUser.setLastLogin(appUserUpdate.getLastLogin());
+       appUser.setFollowing(appUserUpdate.getFollowing());
+       appUser.setFollowers(appUserUpdate.getFollowers());
+       appUser.setPassword(appUserUpdate.getPassword());
+
+       return mapper.appUserToDto(appUser);
     }
 
     @Override
     public void deleteUserById(Long id) {
+        if(!appUserRepo.existsById(id)){
+            //throw error
+        }
 
+        appUserRepo.deleteById(id);
     }
 }
