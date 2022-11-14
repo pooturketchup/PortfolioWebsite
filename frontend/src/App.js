45089a756af48edcf84f3bf9a606841774f8d005
Nolan Mortenson <nolan.mortenson@gmail.com>
2022-11-14 09:24:08 -0500

    created login page for front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
+import {Register} from "./components/Register";
+import {Login} from "./components/Login";
 
 function App() {
+    const [currentForm, setCurrentForm] = useState('login');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
+    }
   return (
     <div className="App">
-
+        {
+            currentForm === 'login' ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+        }
     </div>
   );
 }
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className={'auth-form-container'}>
+            <h2>Register</h2>
+            <form className={'register-form'} onSubmit={handleSubmit}>
+                <label htmlFor={'name'}>Full Name</label>
+                <input onChange={(e) =>setName(e.target.value)} value={name} name={'name'} id={'name'} placeholder={'Full Name'}/>
+                <label htmlFor={'email'}>Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type={'email'} placeholder={'JohnDoe@gmail.com'} id={'email'} name={'email'}/>
+                <label htmlFor={'password'}>Password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type={'password'} placeholder={'********'} id={'password'} name={'password'}/>
+                <button type={'submit'}>Log In</button>
+            </form>
+            <button className={'link-btn'} onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+        </div>
+        )
+}
